5c34b7bca85efb2cc092ba81b40e8e1fda2dc71b
SabaCrevette <sbid.ildr7678@gmail.com>
2024-01-09 02:17:03 +0900

    連携修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
     provider:              'AWS',
     aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: 'us-east-1',
+    region:                ENV['AWS_DEFAULT_REGION'],
     path_style: true
   }
 end
