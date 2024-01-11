0b29c6a5d7af7a6378db14acf1501ce7e0b5222a
SabaCrevette <sbid.ildr7678@gmail.com>
2024-01-11 13:21:23 +0900

    :adhesive_bandage: fix: ブックマーク一覧でもタグ絞り込みできるように修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,9 +1,4 @@
 class BookmarksController < ApplicationController
-  def index
-    @bookmark_posts = current_user.bookmark_posts
-    @context = 'bookmarks'
-  end
-
   def create
     @post = Post.find(params[:post_id])
     current_user.bookmark(@post)
