95f3d3b7466f9247407019f27035b5e16acafd04
SabaCrevette <sbid.ildr7678@gmail.com>
2024-01-15 17:52:52 +0900

    :sparkles: feat: X共有リンクの追加
     本番環境テストのためissueは閉じずに確認

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :prefecture, optional: true
-  has_many :post_tags
+  has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
   has_many :bookmarks, dependent: :destroy
