8410e4605f70e4848bb45928fc04ec3f4e7ead18
SabaCrevette <sbid.ildr7678@gmail.com>
2024-02-18 19:37:58 +0900

    :adhesive_bandage: fix: ページネーションのボタン表示数を調整

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -3,7 +3,7 @@
 Kaminari.configure do |config|
   config.default_per_page = 20
   # config.max_per_page = nil
-  config.window = 1
+  config.window = 5
   config.outer_window = 1
   # config.left = 0
   # config.right = 0
